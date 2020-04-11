29dc7b24616abe1ebede1b3bb18d7e333651ea88
Dizzzmas <yankovskyi.dmytro736@gmail.com>
2020-04-11 22:28:17 +0300

    Log In screen clean up

--- cypress/integration/testSignIn.spec.ts
+++ cypress/integration/testSignIn.spec.ts
@@ -1,37 +1,64 @@
-/// <reference types="cypress" />
-import * as data from "../fixtures/loginFormData.json"
+import { UserFactory } from "../fixtures/fixtureFactory/UserFactory"
+import { IUser } from "model/user"
+
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+const testEmail: string = Cypress.env("testEmail")
+const testPassword: string = Cypress.env("testPassword")
+const UFactory: UserFactory = new UserFactory()
+const okLogInResponse = UFactory.generateLogInResponse()
+const unAuthUserResponse: IUser = UFactory.generateEntry()
 
 describe("Test Sign In", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3001/")
+    cy.server()
+    cy.route("POST", `**/api/auth/refresh`, {}).as("refreshToken")
+    cy.visit("/")
   })
 
-  it("Test success redirect", () => {
-    cy.get("[data-cy-email-input]").type(data.email)
-    cy.get("[data-cy-password-input]").type(data.password)
-    cy.get("[data-cy-login-button").click()
-    cy.location("href").should("eq", "http://localhost:3001/dashboard")
+  it("should redirect to dashboard and set authTokens UI", () => {
+    cy.dataCy("email-input").type(testEmail)
+    cy.route("POST", `**/api/auth/login`, okLogInResponse).as("loginSuccess")
+    cy.dataCy("password-input").type(testPassword)
+    cy.dataCy("login-button")
+      .click()
+      .should(() => {
+        expect(localStorage.getItem("auth-tokens-development")).to.exist.eq(
+          JSON.stringify({ accessToken: okLogInResponse.access_token, refreshToken: okLogInResponse.refresh_token })
+        )
+      })
+    cy.wait("@loginSuccess")
+    cy.url().should("include", "/dashboard")
+  })
+
+  it("should not log in users with invalid email/password", () => {
+    cy.route({
+      method: "POST",
+      url: `**/api/auth/login`,
+      response: unAuthUserResponse,
+      status: 401,
+    }).as("loginUnauthorized")
+    cy.dataCy("email-input").type(testEmail)
+    cy.dataCy("password-input").type(testPassword)
+    cy.dataCy("login-button")
+      .click()
+      .should(() => {
+        expect(localStorage.getItem("auth-tokens-development")).to.not.exist
+      })
+    cy.wait("@loginUnauthorized")
+    cy.url().should("eq", Cypress.config().baseUrl)
   })
 
   it("Test wrong inputs warnings", () => {
-    cy.get("[data-cy-email-input]").type(data.incorrectEmail)
-    cy.get("[data-cy-password-input]").type(data.toShortPassword)
-    cy.get("[data-cy-login-button]").click()
-    cy.get("[data-cy-password-input]").should("contain", "Password should contain at least 6 characters")
-    cy.get("[data-cy-email-input]").should("contain", "Invalid email")
-  })
-
-  it("accepts input", () => {
-    const input = "Some input"
-    cy.get("[data-cy-email-input]").type(input).should("have", input)
-    cy.get("[data-cy-password-input]").type(input).should("have", input)
+    cy.dataCy("email-input").type("invalid-email")
+    cy.dataCy("password-input").type("1")
+    cy.dataCy("login-button").click()
+    cy.dataCy("password-input").should("contain", "Password should contain at least 5 characters")
+    cy.dataCy("email-input").should("contain", "Invalid email")
   })
 
   it("Inputs required", () => {
-    cy.get("[data-cy-login-button]").click()
-    cy.get("[data-cy-email-input]").should("contain", "Required")
-    cy.get("[data-cy-password-input]").should("contain", "Required")
+    cy.dataCy("login-button").click()
+    cy.dataCy("email-input").should("contain", "Required")
+    cy.dataCy("password-input").should("contain", "Required")
   })
 })
-
-export default {}
--- src/pages/Home/signIn.tsx
+++ src/pages/Home/signIn.tsx
@@ -3,10 +3,11 @@ import * as React from "react"
 import { Formik, Form, Field } from "formik"
 import { Button, LinearProgress } from "@material-ui/core"
 import { TextField } from "formik-material-ui"
-import useStyles from "./Styles/signInStyles"
 import * as Yup from "yup"
-import history from "../../History"
-import { useAPICallback } from "../../hooks/useApiCallback"
+import useStyles from "./Styles/signInStyles"
+import { useHistory } from "react-router-dom"
+import { useAPICallback } from "hooks/useApiCallback"
+import { authService, IAuthResponse } from "@jetkit/react"
 
 interface ICredentials {
   email: string
@@ -16,19 +17,21 @@ interface ICredentials {
 const SignInSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
   password: Yup.string()
-    .min(6, "Password should contain at least 6 characters")
+    .min(5, "Password should contain at least 5 characters")
     .max(20, "Password shouldn`t be longer than 20 characters")
     .required("Required"),
 })
 
 export default function SignIn() {
+  const history = useHistory()
+
   const handleSubmit = useAPICallback(
-    () => {
-      console.log("here")
-      history.push("./dashboard")
+    async (values: ICredentials, actions: any) => {
+      await authService.login<IAuthResponse>(values.email, values.password)
+      actions.setSubmitting(false)
+      history.push("/dashboard")
     },
-    [],
-    { rethrowError: true, debounceDelay: 500, onError: (error: Error) => console.log(error) }
+    [history]
   )
 
   const classes = useStyles()
@@ -44,14 +47,21 @@ export default function SignIn() {
       >
         {({ submitForm, isSubmitting, errors, touched }) => (
           <Form className={classes.signInForm}>
-            <Field component={TextField} label="email" name="email" data-cy-email-input variant="outlined" />
-            <Field component={TextField} label="password" name="password" data-cy-password-input variant="outlined" />
+            <Field component={TextField} label="email" name="email" data-cy={"email-input"} variant="outlined" />
+            <Field
+              component={TextField}
+              label="password"
+              name="password"
+              type="password"
+              data-cy={"password-input"}
+              variant="outlined"
+            />
             {isSubmitting && <LinearProgress />}
             <Button
               className={classes.submitButton}
               variant="contained"
               color="primary"
-              data-cy-login-button
+              data-cy={"login-button"}
               disabled={isSubmitting}
               onClick={submitForm}
             >
