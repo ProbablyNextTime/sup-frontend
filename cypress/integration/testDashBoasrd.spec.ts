f0411791c19b1c190032aee73675e852fa916c6a
Shya <rumarenko3@gmail.com>
2020-05-04 22:47:42 +0300

    infinity scroll + search  tests

--- cypress/integration/testDashBoasrd.spec.ts
+++ cypress/integration/testDashBoasrd.spec.ts
@@ -1,35 +1,42 @@
 import { NoticeFactory, IGetNoticesResponse } from "../fixtures/fixtureFactory/NoticeFactory"
+import transportationOffer from "../../src/model/transportationOffer"
 const NFactory: NoticeFactory = new NoticeFactory()
-const getNoticesResponse: IGetNoticesResponse = NFactory.generateGetNoticesResponse()
+const getNoticesResponse: transportationOffer[] = NFactory.generateGetNoticesResponse()
+const getSearchedOffers: transportationOffer[] = NFactory.generateEntries(4)
 
 describe("test dashboard", () => {
   beforeEach(() => {
     cy.server()
-    cy.route("GET", `**/api/notices`, getNoticesResponse).as("getNotices")
-    cy.route("GET", `**/api/transportation_offer`)
+    cy.route("GET", `**/api/transportation_offer?query=&*`, getNoticesResponse).as("getOffers")
     cy.visit("/dashboard")
   })
 
-  it.skip("test infinity scroll", () => {
-    cy.wait("@getNotices")
+  it("test infinity scroll", () => {
+    cy.wait("@getOffers")
     let numberOfChildren = 10
 
-    for (let i = 0; i < 5; i++) {
-      cy.dataCy("notices")
+    for (let i = 0; i < 4; i++) {
+      cy.dataCy("offers")
         .children()
         .then((children) => {
           cy.wrap(children).its("length").should("eq", numberOfChildren)
         })
-      cy.dataCy("notices")
+      cy.dataCy("offers")
         .scrollTo("bottom", { duration: 2000 })
-        .wait("@getNotices")
+        .wait("@getOffers")
         .then(() => (numberOfChildren += 10))
     }
+  })
 
-    it.only("test search", () => {
-      cy.wait("@getNotices")
-
-      cy.dataCy("searchField").type("alg")
-    })
+  it.only("test search", () => {
+    cy.wait("@getOffers")
+    cy.route("GET", `**/api/transportation_offer**`, getSearchedOffers).as("getOffers")
+    cy.dataCy("searchField").type("alg")
+    cy.wait("@getOffers")
+    cy.dataCy("offers")
+      .children()
+      .then((children) => {
+        cy.wrap(children).its("length").should("eq", 4)
+      })
   })
 })
