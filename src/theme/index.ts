035f0b9df1170f13b84a7309fd5d6c352532d000
Shya <rumarenko3@gmail.com>
2020-05-06 19:21:05 +0300

    wip

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -11,7 +11,7 @@ export const theme = createMuiTheme({
   typography: {
     fontFamily: primaryFont,
     subtitle1: {
-      fontFamily : "Lato",
+      fontFamily: "Lato",
       fontSize: 11,
       fontWeight: 500,
       fontStretch: "normal",
@@ -21,8 +21,8 @@ export const theme = createMuiTheme({
       color: "#afafaf",
       padding: "0 0 2px 0",
     },
-
   },
+  //define spacing constants to use inside components
   spacing: [0, 2, 4, 8, 16],
 })
 
