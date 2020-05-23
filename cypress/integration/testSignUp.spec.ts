d02632fa58ccdd70e8c39468b841a9d6174fc51b
Shya <rumarenko3@gmail.com>
2020-05-23 21:50:01 +0300

    final tests fixes

--- cypress/integration/testSignUp.spec.ts
+++ cypress/integration/testSignUp.spec.ts
@@ -3,21 +3,23 @@ import { TransportationOfferFactory } from "../fixtures/fixtureFactory/transport
 import ITransportationOffer from "../../src/model/transportationOffer"
 const transportationOfferFactory: TransportationOfferFactory = new TransportationOfferFactory()
 const getTransportationNoticesResponse: ITransportationOffer[] = transportationOfferFactory.generateGetNoticesResponse()
-
-/* eslint-disable @typescript-eslint/no-unused-expressions */
 const testEmail: string = Cypress.env("testEmail")
 const testPassword: string = Cypress.env("testPassword")
 const UFactory: UserFactory = new UserFactory()
 const signUpResponse = UFactory.generateSignUpResponse(testEmail, testPassword)
+const okLogInResponse = UFactory.generateLogInResponse()
+
 describe("Test Sign Up", () => {
   beforeEach(() => {
     cy.server()
-    cy.route("GET", `**/api/transportation_offer?query=&*`, getTransportationNoticesResponse).as("getOffers")
+    cy.route("POST", `**/api/auth/refresh`, {}).as("refreshToken")
     cy.visit("/")
   })
 
-  it("Test successful sign up", () => {
+  it.only("Test successful sign up", () => {
     cy.route("POST", `**/api/auth/sign-up`, signUpResponse).as("signUpSuccess")
+    cy.route("POST", `**/api/auth/login`, okLogInResponse).as("loginSuccess")
+    cy.route("GET", `**/api/transportation_offer?query=&*`, getTransportationNoticesResponse).as("getOffers")
     cy.dataCy("switch-to-signUp").click()
     cy.dataCy("signUp-button").should("exist")
     cy.dataCy("email-signUp-field").type(testEmail)
@@ -25,6 +27,7 @@ describe("Test Sign Up", () => {
     cy.dataCy("confirm-password-signUp-field").type(testPassword)
     cy.dataCy("signUp-button").click()
     cy.wait("@signUpSuccess")
+    cy.wait("@loginSuccess")
     cy.wait("@getOffers")
     cy.url().should("include", "/dashboard")
   })
