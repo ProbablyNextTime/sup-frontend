c94df95ec48f6d4ba34c0e137bce4f4ba7b2a9e5
Shya <rumarenko3@gmail.com>
2020-05-24 17:58:41 +0300

    fix tests

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,11 +1,5 @@
-import { IAuthResponse, IUser } from "@jetkit/react"
-
-// export interface IUser {
-//   id: ExtId
-//   fullName: string
-//   email: string
-// }
-
-export interface ILoginResponse extends IAuthResponse {
-  user?: IUser
+export interface IUser {
+  id: ExtId
+  fullName: string
+  email: string
 }
