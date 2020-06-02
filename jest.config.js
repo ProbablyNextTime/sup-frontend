3d9f11f94a2f0243165e4c770898ae40d422e1bd
admin <admin@MacBook-Pro-admin.local>
2020-06-02 04:43:20 +0300

    fix: jest

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,11 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/assetsTransformer.ts",
+    "\\.(css|less)$": "<rootDir>/src/assetsTransformer.ts",
+  },
 
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
